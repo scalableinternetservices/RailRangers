f5260da89c26297aac4cea814b49f84ade2393c5
krishna-ucsb <krishnachaitanya@umail.ucsb.edu>
2019-11-15 23:19:20 -0800

    integrating user with post and comments

--- db/migrate/201911120353118_create_comments.rb
+++ db/migrate/201911120353118_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :user
-      t.string :comment
+      t.text :comment
       t.references :post, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20191112035317_create_posts.rb
+++ db/migrate/20191112035317_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :user
-      t.string :message
+      t.text :message
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
