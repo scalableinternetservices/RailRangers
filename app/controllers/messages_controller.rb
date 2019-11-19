2ad4b53b405288d9580949cb31a0bb9b8156f70e
krishna-ucsb <krishnachaitanya@umail.ucsb.edu>
2019-11-19 10:18:47 -0800

    Fixed conversation issues

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,18 @@
 class MessagesController < ApplicationController
   def create
     @conversation = Conversation.find(params[:conversation_id])
-    @message = @conversation.messages.build(message_params)
+    @message = @conversation.messages.create(message_params)
     @message.user_id = current_user.id
     @message.save!
 
-    @path = conversation_path(@conversation)
+    respond_to do |format|
+      format.html { redirect_to @conversation }
+    end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:body)
+    params.require(:message).permit(:body, :conversation_id)
   end
 end
