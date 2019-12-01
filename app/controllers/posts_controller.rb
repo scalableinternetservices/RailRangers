e711907425f283263c7fa294927ea69fc065b432
benu <benuchangmai@gmail.com>
2019-11-30 19:40:14 -0800

    Optimized for n+1 query

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 50).order("created_at DESC")
+    @posts = Post.includes(:comments).paginate(:page => params[:page], :per_page => 50).order("created_at DESC")
     @comment = Comment.new
     @post = Post.new
   end
