8d6e1867f90517a3928ca2b2cdf64b669ef0221f
benu <benuchangmai@gmail.com>
2019-11-30 16:38:40 -0800

    Fixed bootstrap issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require bootstrap-sprockets
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap")
+
 
 
 
