a347745fb02e40e2f8cdd3ecc527b82c73e349ed
benu <benuchangmai@gmail.com>
2019-12-01 20:19:07 -0800

    Optimized /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
 	    # @users = User.all
 	    @users = User.where('id != ?' , current_user.id).paginate(:page => params[:page], :per_page => 50)
 	    @friends = current_user.friends
+	    @requesters = current_user.requesters
+		@requestees = current_user.requestees
+		@received_requests = current_user.received_requests
   	end
 	def show
 		@user = User.find_by(id: params[:id])
