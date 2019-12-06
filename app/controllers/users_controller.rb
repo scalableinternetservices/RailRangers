05aa05f4de580e804d33f7741f0f71c60eae5672
benu <benuchangmai@gmail.com>
2019-12-05 17:51:34 -0800

    Added search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,17 @@ class UsersController < ApplicationController
 	skip_before_action :authorized, only: [:show, :index, :new, :edit, :create]
 	def index
 	    # @users = User.all
-	    @users = User.where('id != ?' , current_user.id).paginate(:page => params[:page], :per_page => 50)
-	    @friends = current_user.friends
+		if params[:search]
+			@users = User.where('username LIKE ?', "%#{params[:search]}%").paginate(:page => params[:page], :per_page => 50)
+			
+		else
+			@users = User.where('id != ?' , current_user.id).paginate(:page => params[:page], :per_page => 50)
+		end
+		@friends = current_user.friends
 	    @requesters = current_user.requesters
 		@requestees = current_user.requestees
 		@received_requests = current_user.received_requests
+	    
   	end
 	def show
 		@user = User.find_by(id: params[:id])
